cfe7ac42cbc5237eeba92de7f5c33f7076b4c8bf
damienzonly <damianomails@gmail.com>
2023-04-08 01:05:23 +0200

    using begin to fetch first host

--- Source/Settings/Settings.cpp
+++ Source/Settings/Settings.cpp
@@ -8,12 +8,10 @@ void Settings::init() {
     juce::AudioDeviceManager deviceManager;
     deviceManager.initialiseWithDefaultDevices(0, 2);
     const juce::OwnedArray<juce::AudioIODeviceType> &deviceTypes = deviceManager.getAvailableDeviceTypes();
-    for (auto &deviceType: deviceTypes) {
-        cout << "typename: " << deviceType->getTypeName() << endl;
-        deviceType->scanForDevices();
-        const juce::StringArray names = deviceType->getDeviceNames();
-        for (auto name: names) {
-            cout << "device name: " << name.toRawUTF8() << endl;
-        }
+    const auto &host = *deviceTypes.begin();
+    host->scanForDevices();
+    const juce::StringArray names = host->getDeviceNames();
+    for (auto name: names) {
+        cout << "device name: " << name << endl;
     }
 }
